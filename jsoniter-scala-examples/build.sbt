77ba848a57d40469965af7f362426397fd75bcd8
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-07-07 18:15:50 +0200

    Try to run with ZIO

--- jsoniter-scala-examples/build.sbt
+++ jsoniter-scala-examples/build.sbt
@@ -5,6 +5,7 @@ val `jsoniter-scala-examples` = project.in(file("."))
     crossScalaVersions := Seq("2.13.3", "2.12.11", "2.11.12"),
     mainClass in assembly := Some("com.github.plokhotnyuk.jsoniter_scala.examples.Example01"),
     libraryDependencies ++= Seq(
+      "dev.zio" %% "zio" % "1.0.0-RC21-2",
       "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "latest.integration" % "runtime-internal",
       "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "latest.integration" % "compile-internal" // or "provided", but it is required only in compile-time
     ) ++ (CrossVersion.partialVersion(scalaVersion.value) match {
--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -3,21 +3,27 @@ package com.github.plokhotnyuk.jsoniter_scala.examples
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
 
+import zio.console._
+
 /**
   * Example of basic usage from README.md
   */
-object Example01 {
+object Example01 extends zio.App {
   case class Device(id: Int, model: String)
 
   case class User(name: String, devices: Seq[Device])
 
   implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make
 
-  def main(args: Array[String]): Unit = {
+  def run(args: List[String]) = myAppLogic.exitCode
+
+  val myAppLogic = {
     val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
     val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
 
-    println(user)
-    println(new String(json, "UTF-8"))
+    for {
+      _    <- putStrLn(user.toString)
+      _    <- putStrLn(new String(json, "UTF-8"))
+    } yield ()
   }
 }

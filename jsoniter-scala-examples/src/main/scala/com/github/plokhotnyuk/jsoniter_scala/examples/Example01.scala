961968428aea5e2749fe82435a1631e848d5afd6
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-02-07 10:19:36 +0100

    An example with a custom codec injection

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -1,23 +1,44 @@
 package com.github.plokhotnyuk.jsoniter_scala.examples
 
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, _}
+import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
 
 /**
   * Example of basic usage from README.md
   */
 object Example01 {
-  case class Device(id: Int, model: String)
+  case class ComputationSpeed(value: Double, unit: ComputationSpeedUnit)
 
-  case class User(name: String, devices: Seq[Device])
+  object ComputationSpeed {
+    def symbolToUnit(x: String): Option[ComputationSpeedUnit] = x match {
+      case "IPC" => Some(ComputationSpeedUnit("IPC"))
+      case "CPI" => Some(ComputationSpeedUnit("CPI"))
+      case _ => None
+    }
+  }
 
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make
+  case class ComputationSpeedUnit(symbol: String)
+
+  object ComputationSpeedUnit {
+    implicit val codec: JsonValueCodec[ComputationSpeedUnit] = new JsonValueCodec[ComputationSpeedUnit] {
+      override def encodeValue(x: ComputationSpeedUnit, out: JsonWriter): Unit = out.writeVal(x.symbol)
+
+      override def decodeValue(in: JsonReader, default: ComputationSpeedUnit): ComputationSpeedUnit = {
+        val x = in.readString(null)
+        ComputationSpeed.symbolToUnit(x).getOrElse(in.decodeError(s"Unsupported unit of measure: $x"))
+      }
+
+      override val nullValue: ComputationSpeedUnit = null
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    implicit val codec: JsonValueCodec[ComputationSpeed] = JsonCodecMaker.make
 
-    println(user)
+    val data = readFromArray[ComputationSpeed]("""{"value":3.0,"unit":"IPC"}""".getBytes("UTF-8"))
+    val json = writeToArray(ComputationSpeed(0.25, ComputationSpeedUnit("CPI")))
+
+    println(data)
     println(new String(json, "UTF-8"))
   }
 }

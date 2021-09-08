d72b8dbf00d390e83784e1f93b7796798bf74534
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2021-09-08 09:13:19 +0200

    Results of the 2067th pull request for GraaVM CE

--- build_chart.sh
+++ build_chart.sh
@@ -1,19 +1,9 @@
-echo 'var providedBenchmarks = ["Corretto 8", "Corretto 11", "OpenJDK 18", "GraalVM CE 11", "GraalVM CE 16", "GraalVM EE 8", "GraalVM EE 11", "GraalVM EE 16"];
+echo 'var providedBenchmarks = ["OpenJDK 17", "GraalVM CE Java 17", "GraalVM CE Java 17 patched"];
 var providedBenchmarkStore = {
-"Corretto 8":'
-jq -c . < corretto-8.json
-echo ', "Corretto 11":'
-jq -c . < corretto-11.json
-echo ', "OpenJDK 18":'
-jq -c . < openjdk-18.json
-echo ', "GraalVM CE 11":'
-jq -c . < graalvm-ce-java11.json
-echo ', "GraalVM CE 16":'
-jq -c . < graalvm-ce-java16.json
-echo ', "GraalVM EE 8":'
-jq -c . < graalvm-ee-java8.json
-echo ', "GraalVM EE 11":'
-jq -c . < graalvm-ee-java11.json
-echo ', "GraalVM EE 16":'
-jq -c . < graalvm-ee-java16.json
+"OpenJDK 17":'
+jq -c . < openjdk-17.json
+echo ', "GraalVM CE Java 17":'
+jq -c . < graalvm-ce-java17.json
+echo ', "GraalVM CE Java 17 patched":'
+jq -c . < graalvm-ce-java17-patched.json
 echo '}'
